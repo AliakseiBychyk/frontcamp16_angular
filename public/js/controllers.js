a67a03a35825dddcc2e91593de889d54bf6fe2da
Aliaksei Bychyk <aliaksei_bychyk@epam.com>
2017-01-31 10:32:15 +0300

    model and factory

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,43 +1,31 @@
 exports.mainController = ($scope) => {
-  $scope.title = 'Angular + Webpack';
-  $scope.greetings = 'Aleks\'s Blog';
+    $scope.title = 'Angular + Webpack';
+    $scope.greetings = 'Aleks\'s Blog';
 };
 
 exports.authController = ($scope) => {
-  $scope.title = 'Authentification';
+    $scope.title = 'Authentification';
 };
 
 exports.postController = ($scope) => {
-  $scope.title = 'Add new Post';
+    $scope.title = 'Add new Post';
 };
 
 exports.blogController = ($scope) => {
-  $scope.title = 'Aleks\'s Blog';
+    $scope.title = 'Aleks\'s Blog';
 };
 
-exports.PostsController = ($scope) => { 
- 
-  const getPosts = (callback) => {
-    fetch('http://localhost:8000/blog/json')
-      .then(response => response.json())
-      .then((data) => {
-        callback(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+// exports.PostsController = ($scope, DataPosts) => {
+//     $scope.posts = DataPosts.getPosts();
+//     console.log($scope.posts);
+//     $scope.posts.forEach(post => console.log(post.title));
+// };
 
-  getPosts( data => {
-    $scope.posts = data.posts
+exports.PostsController = ($scope, model) => {
+
+    $scope.posts = model;
+
+    console.log(model);
     $scope.posts.forEach(post => console.log(post.title));
-  }); 
-  
-  
-  // $scope.posts = getPosts(data => data.posts);
-  // $scope.posts.forEach(post => console.log(post.title));
- 
+
 };
-
-
-// exports.PostsController = funcion($scope, $http)
--- public/js/index.js
+++ public/js/index.js
@@ -6,47 +6,68 @@ import controllers from './controllers';
 
 const components = angular.module('frontcamp16.components', ['ng']);
 
-console.log(angular);
-
 _.each(controllers, (controller, name) => {
-  components.controller(name, controller);
-  console.log(name); // => authController
-  console.log(controller); // => function ($scope) {$scope.title = 'Authentification';}
+    components.controller(name, controller);
+    console.log(name); // => authController
+    console.log(controller); // => function ($scope) {$scope.title = 'Authentification';}
 })
 
 _.each(directives, (directive, name) => {
-  components.directive(name, directive);
-  console.log(name);
-  console.log(directive);
+    components.directive(name, directive);
+    console.log(name);
+    console.log(directive);
 });
-
 
 const app = angular.module('frontcamp16', ['frontcamp16.components', 'ngRoute']);
 
-console.log(ngRoute); // => 'ngRoute'
-console.log(ngRoute === 'ngRoute'); // => true
-console.log(app); // => Object {...}
-
 app.config(($routeProvider) => {
-  $routeProvider.
-    when('/', {
-      templateUrl: 'templates/home.html',
-      controller: 'mainController'
-    }).
-    when('/login', {
-      templateUrl: 'templates/login.html',
-      controller: 'authController'
-    }).
-    when('/register', {
-      templateUrl: 'templates/register.html',
-      controller: 'authController'
-    }).
-    when('/blog', {
-      templateUrl: 'templates/blog.html',
-      controller: 'PostsController'
-    }).
+    $routeProvider
+        .when('/', {
+            templateUrl: 'templates/home.html',
+            controller: 'mainController'
+        })
+        .when('/login', {
+            templateUrl: 'templates/login.html',
+            controller: 'authController'
+        })
+        .when('/register', {
+            templateUrl: 'templates/register.html',
+            controller: 'authController'
+        })
+        .when('/blog', {
+            templateUrl: 'templates/blog.html',
+            controller: 'PostsController'
+        }).
     when('/newpost', {
-      templateUrl: 'templates/newpost.html',
-      controller: 'postController'
+        templateUrl: 'templates/newpost.html',
+        controller: 'postController'
     });
 });
+
+// app.factory('DataPosts', () => {
+//     var posts = {};
+//     fetch('http://localhost:8000/blog/json')
+//         .then(response => response.json())
+//         .then((data) => {
+//             posts = data.posts;
+//             posts.forEach(post => console.log(post.title));
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+//     return {
+//         getPosts: () => posts
+//     }
+// })
+
+app.value('model', () => {
+    fetch('http://localhost:8000/blog/json')
+        .then(response => response.json())
+        .then((data) => {
+            return data.posts;
+            data.posts.forEach(post => console.log(post.title));
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
