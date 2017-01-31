7fd22d1749ae705479061e866310441c56082364
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-01-31 16:42:43 +0300

    erased commented old code

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -15,62 +15,6 @@ exports.BlogController = ($scope) => {
     $scope.title = 'Aleks\'s Blog';
 };
 
-// exports.PostsController = ($scope, DataPosts) => {
-//     $scope.posts = DataPosts.getPosts();
-//     console.log($scope.posts);
-//     $scope.posts.forEach(post => console.log(post.title));
-// };
-
-// exports.PostsController = ($scope) => {
-//     function getData() {
-//         fetch('http://localhost:8000/blog/json')
-//             .then(response => response.json())
-//             .then((data) => {
-//                 data.posts.forEach(post => console.log(post.title));
-//                 updateScope(data);
-//             })
-//             .catch((err) => {
-//                 console.log(err);
-//             })
-//     }
-
-//     function updateScope(method) {
-//         $scope.method = method;
-//     }
-    
-//     getData();
-//     console.log($scope);
-// };
-
-// exports.PostsController = ($scope) => {
-
-//     $scope.data = (() => {
-//         fetch('http://localhost:8000/blog/json')
-//             .then(response => response.json())
-//             .then((data) => {
-//                 getPosts(data);
-//                 getPosts(data).posts.forEach(post => console.log(post.title));
-//             })
-//             .catch((err) => {
-//                 console.log(err);
-//             })
-//     })();
-
-//     const getPosts = json => json;
-
-//     console.log($scope.data);
-
-// };
-
-
-// exports.PostsController = ($scope, $http) => {
-//   $http.get('http://localhost:8000/blog/json')
-//     .success((data) => {
-//       $scope.posts = data;
-//     })
-//     .error((err) => console.log(err));
-// });
-
 exports.PostsController = ($scope) => {
 
     $scope.load = () => {
